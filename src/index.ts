2286015e4d7c48d65548f3c1d216653ef3e336ce
Katsukiniwa <katsukiniwa0217@gmail.com>
2021-06-19 20:19:17 +0900

    update get sample endpoint

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,10 @@ app.get("/", (_request, response) => {
   response.send("Hello express ecs");
 });
 
+app.get("/sample", (_request, response) => {
+  response.send("sample");
+});
+
 app.listen(8080, () => {
   console.log("listen on 8080");
 });
