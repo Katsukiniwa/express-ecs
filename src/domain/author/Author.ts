6d6239d7a7551d64deffb69c9a3ed774e6f76058
Katsukiniwa <katsukiniwa0217@gmail.com>
2021-07-03 21:34:36 +0900

    ドメイン層に追加

--- src/domain/author/Author.ts
+++ src/domain/author/Author.ts
@@ -1,18 +1,34 @@
 import { Entity } from "../../common/domain/Entity";
 import { AuthorName } from "./AuthorName";
 
+/**
+ * @name 著者
+ * @description 著者は名前・アイコン・紹介文をもつ
+ */
 export class Author extends Entity {
   public readonly id: number | null;
   private readonly _name: AuthorName;
+  private readonly _description: string;
+  private readonly _icon: string;
 
-  constructor(id: number | null, name: string) {
+  constructor(id: number | null, name: string, description: string, icon: string) {
     super();
 
     this.id = id;
-    this._name = name;
+    this._name = new AuthorName(name);
+    this._description = description;
+    this._icon = icon;
   }
 
-  get name(): any {
+  get name(): AuthorName {
     return this._name;
+  }
+
+  get description(): string {
+    return this._description;
+  }
+
+  get icon(): string {
+    return this._icon;
   }
 }
--- src/domain/novel/Novel.ts
+++ src/domain/novel/Novel.ts
@@ -1,18 +1,36 @@
 import { Entity } from "../../common/domain/Entity";
+import { Author } from "../author/Author";
+import { Section } from "../section/Section";
 import { NovelTitle } from "./NovelTitle";
 
+/**
+ * @name 小説
+ * @description 小説はタイトル・章・あとがき・執筆者を含む
+ */
 export class Novel extends Entity {
   public readonly id: number | null;
   private readonly _title: NovelTitle;
+  private readonly _sections: Section[];
+  private readonly _author: Author;
 
-  constructor(id: number | null, title: string) {
+  constructor(id: number | null, title: string, sections: Section[], author: Author) {
     super();
 
     this.id = id;
     this._title = new NovelTitle(title);
+    this._sections = sections;
+    this._author = author;
   }
 
   get title(): string {
     return this._title.value;
+  }
+
+  get sections(): Section[] {
+    return this._sections;
+  }
+
+  get author(): Author {
+    return this._author;
   }
 }
